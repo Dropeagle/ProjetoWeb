fb3e8731a34a697a6d9fb4610fba9b0ff689a22c
Dropeagle <papagodao.gota@gmail.com>
2025-09-29 08:56:13 -0300

    ajustes

--- js/login.js
+++ js/login.js
@@ -1,16 +1,15 @@
-// Escuta o envio do formulário de login
 document.getElementById("loginForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // impede o recarregamento da página
+  e.preventDefault(); 
 
   const email = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  console.log("Tentativa de login:", email, password); // debug no console
+  console.log("Tentativa de login:", email, password); 
 
-  // Validação simples com email e senha
+  
   if (email === "admin@swift.com" && password === "1234") {
     alert("✅ Login bem-sucedido!");
-    window.location.href = "home.html"; // redireciona para o dashboard
+    window.location.href = "home.html"; 
   } else {
     alert("❌ Email ou senha incorretos.");
   }
